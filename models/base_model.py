29c9d8aa09d75a82bb519ca3069023f09c18017c
Orcha02 <orcha2002@gmail.com>
2021-06-25 01:21:38 +0000

    Add README

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class BaseModel defines all common attributes/methods for other classes """
+
+
+class BaseModel:
+    """Class for base model"""
+
+    def __init__(self):
+        """Initialization"""
+        self.
+
+     def __str__(self):
+        """str format"""
+        return "[{}] ({}) {}"
