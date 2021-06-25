de49ca4cc1bf841a0ae5581f398d358619076e11
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-06-25 13:20:19 -0500

    Update de Base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,20 @@ from datetime import datetime
 class BaseModel:
     """ Class for base model """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialization """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
     def __str__(self):
         """ Str format for references """
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
