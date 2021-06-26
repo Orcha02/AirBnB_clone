97a52e9f508981ef78217cbadf47b7a460d70686
Orcha02 <orcha2002@gmail.com>
2021-06-26 03:39:00 +0000

    Organize to start Console 0.1 and add user.py file

--- console.py
+++ console.py
@@ -14,6 +14,41 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
         return True
 
+    def do_create(self, line):
+        """
+        Creates a new instance of BaseModel, saves it
+        (to the JSON file) and prints the id
+        """
+        pass
+
+    def do_show(self, line):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        pass
+
+    def do_destroy(self, line):
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file)
+        """
+        pass
+
+    def do_all(self, line):
+        """
+        Prints all string representation of all
+        instances based or not on the class name
+        """
+        pass
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file)
+        """
+        pass
+
     def emptyline(self):
         """Print a new empty line"""
         if self.lastcmd:
