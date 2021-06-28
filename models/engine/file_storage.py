76bcd192e8d217ce1ae409bae859133555696e82
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-06-28 12:30:03 -0500

    Fix the files base_model and file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,8 @@
 """ CLass FileStorage serializes instances to a JSON file and deserializes JSON file to instances """
 
 import json
-import models
+from models.base_model import BaseModel
+
 
 class FileStorage:
     '''Serializes instances to JSON file and deserializes to JSON file.
@@ -12,13 +13,13 @@ class FileStorage:
 
     def all(self):
         ''' Return the dictionary '''
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         '''
             Set in __objects the obj with key <obj class name>.id 
         '''
-        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
@@ -38,11 +39,9 @@ class FileStorage:
             Deserializes the JSON file to __objects.
         '''
         try:
-            with open(FileStorage.__file_path, encoding="utf-8") as f:
-                FileStorage.__objects = json.load(f)
-            for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                class_name = models.classes[class_name]
-                FileStorage.__objects[key] = class_name(**val)
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, val in (json.load(f)).items():
+                    val = eval(val["__class__"])(**val)
+                    self.__objects[key] = val
         except FileNotFoundError:
             pass
