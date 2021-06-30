fbfaa0fd3842cbffa40e6a9c132d2115be326965
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-06-30 16:10:30 -0500

    Remove garbage files

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """ Initialization """
-        if (len(kwargs) == 0):
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
